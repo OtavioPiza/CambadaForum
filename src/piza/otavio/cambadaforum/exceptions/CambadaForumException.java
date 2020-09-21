a916fbe89fa3c42c002bacd207963f1d5360a735
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-09-21 18:27:12 -0300

    Done!

--- src/piza/otavio/cambadaforum/exceptions/CambadaForumException.java
+++ src/piza/otavio/cambadaforum/exceptions/CambadaForumException.java
@@ -8,9 +8,14 @@ package piza.otavio.cambadaforum.exceptions;
  */
 public class CambadaForumException extends Exception{
 	private static final long serialVersionUID = 1L; // Serial UID
-	protected final String message;
+	protected final String message; // Stores exception message
 	
+	/**
+	 * Constructor that allows for the user to add a message to the exception
+	 * 
+	 * @param message that will be part of the exception
+	 */
 	CambadaForumException(String message) {
 		this.message = message;
-	}
-}
+	} // CambadaForumException(...)
+} // CambadaForumException
