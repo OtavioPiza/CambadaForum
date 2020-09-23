c5fe48d66886794811391c11912744665a5f41ba
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-09-23 10:35:10 -0300

    Done!

--- src/piza/otavio/cambadaforum/AppCommands.java
+++ src/piza/otavio/cambadaforum/AppCommands.java
@@ -15,17 +15,31 @@ public enum AppCommands {
 	QUIT("quit", "closes the program"),
 	RESETDATABASE("reset", "resets the postgresql database");
 	
-	private String command;
-	private String description;
+	private String command;		// Stores the command to use the functionality
+	private String description;	// Stores the functionality description
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param command
+	 * @param description
+	 */
 	AppCommands(String command, String description) {
 		this.command = command;
 		this.description = description;
-	}
+	} // End AppCommands
+	
+	/**
+	 * @return command
+	 */
 	public String getCommand() {
 		return command;
-	}
+	} // End getCommand()
+	
+	/**
+	 * @return command functionality description
+	 */
 	public String getDescription() {
 		return description;
-	}
+	} // End getDescription()
 } // End AppCommands
